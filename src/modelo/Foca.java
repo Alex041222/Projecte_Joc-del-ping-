03dab029e7ded1492c976d63f338b1d4da843805
Your Name <you@example.com>
2025-04-03 17:56:57 +0200

    Revert "Squashed commit of the following:"
    This reverts commit 566d1e07dc6b408e83dc101e2bcca566d619a487.

--- src/modelo/Foca.java
+++ src/modelo/Foca.java
@@ -1,28 +1,5 @@
 package modelo;
 
-public class Foca extends Jugador {
-	private boolean soborno;
-	    
-	public Foca(String Nombre, String Color, int Posicion, boolean soborno) {
-	    super(Nombre, Color, Posicion); // Llamada al constructor de Jugador
-	    this.soborno = soborno;
-	}
-	    
-	public boolean getSoborno() {
-	    return soborno;
-	}
-	    
-	public void setSoborno(boolean soborno) {
-	    this.soborno = soborno;
-	}
-	    
-	public void aplastarJugador(Pinguino p) {
-	    // Implementa la lógica para aplastar a un pingüino
-	    System.out.println("La foca aplasta al pingüino " + p.getNombre());
-	}
-	    
-	public void golpearJugador(Pinguino p) {
-	    // Implementa la lógica para golpear a un pingüino
-	    System.out.println("La foca golpea al pingüino " + p.getNombre());
-	}
+public class Foca {
+
 }
