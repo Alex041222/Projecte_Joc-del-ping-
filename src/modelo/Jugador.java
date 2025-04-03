bc09700d48047a89c9d489dbb8e2e98a685d6b27
Your Name <you@example.com>
2025-04-03 17:57:59 +0200

    Revert "Merge branch 'main' into Kevin_Item"
    This reverts commit 3dfdce7334a37732192e17ea3fb0547f3441d765, reversing
    changes made to 03dab029e7ded1492c976d63f338b1d4da843805.

--- src/modelo/Jugador.java
+++ src/modelo/Jugador.java
@@ -1,41 +1,5 @@
 package modelo;
 
 public class Jugador {
-	private int Posicion;
-	private String Nombre;
-	private String Color;
-	
-	 public Jugador( int Posicion, String Nombre,String color) {
-			this.setPosicion(Posicion);
-			this.setNombre(Nombre);
-			this.setColor(color);
-	    }
 
-	public int getPosicion() {
-		return Posicion;
-	}
-
-	public void setPosicion(int Posicion) {
-		this.Posicion = Posicion;
-	}
-
-	public String getNombre() {
-		return Nombre;
-	}
-
-	public void setNombre(String Nombre) {
-		this.Nombre = Nombre;
-	}
-
-	public String getColor() {
-		return Color;
-	}
-
-	public void setColor(String Color) {
-		this.Color = Color;
-	}
-	
-	public void tirarDado(int maximoDado) {
-		
-	}
 }
--- src/modelo/Pingüino.java
+++ src/modelo/Pingüino.java
@@ -1,42 +1,5 @@
 package modelo;
 
-public class Pingüino extends Jugador {
-	private Inventario inventario;
-	
-	public Pingüino (Int Posicion, String Nombre, String Color);
-		super(Nombre, Color, Posicion); // Llamo al constructor del Jugadr
-		this.inventario = new Inventario();	
-	}
+public class Pingüino {
 
-	public Inventario getInventario() {
-		return inventario;
-	}
-	
-	public void setInventario(Inventario inventario) {
-		this.inventario = inventario;
-	}
-	
-	public void gestionarBatalla() {
-	   // Implementa lógica para gestionar batalla
-	    System.out.println("Gestionando la batalla...");
-	}
-	    
-	public void usarObjeto() {
-	   // Implementa lógica para usar un objeto del inventario
-	    System.out.println("Usando objetos del inventario...");
-    }
-	    
-	public void añadirItem(Item item) {
-	   // Implementa lógica para añadir un item al inventario
-	    System.out.println("Añadiendo item: " + item.getNombre());
-	    inventario.agregarItem(item);
-	}
-	    
-	public void quitarItem(Item item) {
-	   // Implementa lógica para quitar un item del inventario
-	    System.out.println("Quitando item: " + item.getNombre());
-	    inventario.eliminarItem(item);
-	}
-	
-}
 }
