5e2e2ecdd687135171372b1dcf5bd8ffa4fd3352
Your Name <you@example.com>
2025-04-03 17:11:48 +0200

    Merthan Bilgin
    He hecho la clase inventario

--- src/modelo/Inventario.java
+++ src/modelo/Inventario.java
@@ -1,5 +1,35 @@
 package modelo;
 
-public class Inventario {
+import java.util.ArrayList;
 
+public class Inventario {
+	
+	private ArrayList<Item> lista; // Lista para guardar los items
+
+    // Constructor: Inicializa el inventario como una lista vacía
+    public Inventario() {
+        this.lista = new ArrayList<>();
+    }
+
+    // Para obtener la lista de items
+    public ArrayList<Item> getLista() {
+        return lista;
+    }
+
+    // Para establecer una nueva lista de items
+    public void setLista(ArrayList<Item> lista) {
+        this.lista = lista;
+    }
+
+    // Método para agregar un item al inventario
+    public void agregarItem(Item item) {
+        lista.add(item);
+    }
+
+    // Para mostrar los items del inventario
+    public void mostrarInventario() {
+        for (Item item : lista) {
+            System.out.println(item);
+        }
+    }
 }
