cd4a6f31073e5a54f4fe834f11bb8cd472a8a788
Your Name <you@example.com>
2025-04-10 16:55:35 +0200

    Merthan Bilgin
    He creado el código de la clase oso.

--- src/modelo/Oso.java
+++ src/modelo/Oso.java
@@ -1,5 +1,15 @@
 package modelo;
 
 public class Oso {
+	
+	// Constructor
+    public Oso() {
+        System.out.println("Un oso ha aparecido en el tablero.");
+    }
 
+    // Si el oso ataca a un jugador ese jugador vuelve al inicio
+    public void realizarAcción(Jugador jugador) {
+        jugador.volverAlInicio();
+        System.out.println(jugador.getNombre() + " has sido atacado por un oso y por lo tanto vuelves al inicio :(.");
+    }
 }
