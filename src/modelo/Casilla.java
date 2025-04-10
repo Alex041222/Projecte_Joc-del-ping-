398b07369b31ed3418112fab00eece4c9bd86fbb
Your Name <you@example.com>
2025-04-10 16:58:37 +0200

    casillas final

--- src/modelo/Casilla.java
+++ src/modelo/Casilla.java
@@ -1,5 +1,44 @@
 package modelo;
 
-public class Casilla {
+import java.util.ArrayList;
 
+public abstract class Casilla {
+	
+	private int Posicion;
+	
+	private ArrayList<Jugador>jugadoresActuales;
+
+	public Casilla(int Posicion,ArrayList<Jugador> jugadoresActuales) {
+		this.Posicion = Posicion;
+		this.jugadoresActuales = new ArrayList<>();		
+	}
+	public int getPosicion() {
+		return Posicion;
+	}
+
+	public void setPosicion(int posicion) {
+		this.Posicion = posicion;
+	}
+
+	public ArrayList<Jugador> getJugadoresActuales() {
+		return jugadoresActuales;
+	}
+
+	public void setJugadoresActuales(ArrayList<Jugador> jugadoresActuales) {
+		this.jugadoresActuales = jugadoresActuales;
+	}
+	
+	public abstract void realizarAccion();{
+		
+	}
+	
+	public abstract void añadirjugador(Jugador j);{
+		
+	}
+	public abstract void quitarjugador(Jugador j);{
+		
+	}
+	
 }
+ 
+	
