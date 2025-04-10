79e9f61b8a99d8b76eb5fe6aa76a4e0e99953416
KevinAngelP <kevinangelperez71@alumnes.ilerna.com>
2025-04-10 16:35:39 +0200

    Update Trineo.java

--- src/modelo/Trineo.java
+++ src/modelo/Trineo.java
@@ -2,4 +2,20 @@ package modelo;
 
 public class Trineo {
 
+    private String descripcion;
+
+    public Trineo(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void accion() {
+    }
 }
