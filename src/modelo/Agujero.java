05861e908e77665b44307044e2b82278716c0479
KevinAngelP <kevinangelperez71@alumnes.ilerna.com>
2025-04-10 16:30:58 +0200

    Update Agujero.java

--- src/modelo/Agujero.java
+++ src/modelo/Agujero.java
@@ -2,4 +2,21 @@ package modelo;
 
 public class Agujero {
 
+    private String descripcion;
+
+    public Agujero(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void accion() {
+    	
+    }
 }
