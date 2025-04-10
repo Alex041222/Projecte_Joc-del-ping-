776d8fdce27873068121be39f826a11e6371f082
Your Name <you@example.com>
2025-04-10 16:56:01 +0200

    update pinguino

--- src/modelo/Pingüino.java
+++ src/modelo/Pingüino.java
@@ -4,7 +4,6 @@ package modelo;
 		private Inventario inventario;
 		
 		public Pingüino(int Posicion, String Nombre, String Color) {
-<<<<<<< Updated upstream
 			super(Posicion, Color, Nombre); // Llamo al constructor del Jugador
 			this.inventario = new Inventario();	
 		}
@@ -40,39 +39,3 @@ package modelo;
 		}
 	}
 
-=======
-			super(Nombre, Color, Posicion); // Llamo al constructor del Jugador
-			this.inventario = new Inventario();	
-		}
-
-		public Inventario getInventario() {
-			return inventario;
-		}
-		
-		public void setInventario(Inventario inventario) {
-			this.inventario = inventario;
-		}
-		
-		public void gestionarBatalla() {
-		    // Implementa lógica para gestionar batalla
-		    System.out.println("Gestionando la batalla...");
-		}
-		    
-		public void usarObjeto() {
-		    // Implementa lógica para usar un objeto del inventario
-		    System.out.println("Usando objetos del inventario...");
-	    }
-		    
-		public void añadirItem(Item item) {
-		    // Implementa lógica para añadir un item al inventario
-		    System.out.println("Añadiendo item: " + item.getNombre());
-		    inventario.agregarItem(item);
-		}
-		    
-		public void quitarItem(Item item) {
-		    // Implementa lógica para quitar un item del inventario
-		    System.out.println("Quitando item: " + item.getNombre());
-		    inventario.eliminarItem(item);
-		}
-	}
->>>>>>> Stashed changes
