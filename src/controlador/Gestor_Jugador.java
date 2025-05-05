f51e8461263f830ccd2baf6e415c824e49ef23dd
Your Name <you@example.com>
2025-05-05 16:55:59 +0200

    primera actualitzacio

--- src/controlador/Gestor_Jugador.java
+++ src/controlador/Gestor_Jugador.java
@@ -1,5 +1,53 @@
 package controlador;
 
-public class Gestor_Jugador {
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gestor_Jugador {
+	   // Llista de jugadors
+    private final List<Jugador> jugadors = new ArrayList<>();
+
+    // Classe interna per a Jugador (sense getters/setters)
+    public static class Jugador {
+        public final String nom;
+        public int puntuacio;
+        
+        public Jugador(String nom, int puntuacio) {
+            this.nom = nom;
+            this.puntuacio = puntuacio;
+        }
+    }
+
+    // Singleton
+    private static Gestor_Jugador instancia;
+    public static Gestor_Jugador getInstancia() {
+        if (instancia == null) {
+            instancia = new Gestor_Jugador();
+        }
+        return instancia;
+    }
+
+    // Afegir jugador
+    public void afegirJugador(String nom, int puntuacio) {
+        jugadors.add(new Jugador(nom, puntuacio));
+    }
+
+    // Obtenir llista de jugadors (còpia)
+    public List<Jugador> getJugadors() {
+        return new ArrayList<>(jugadors);
+    }
+
+    // Eliminar jugador (sense break)
+    public void eliminarJugador(String nom) {
+        boolean trobat = false;
+        int i = 0;
+        while (!trobat && i < jugadors.size()) {
+            if (jugadors.get(i).nom.equals(nom)) {
+                jugadors.remove(i);
+                trobat = true;
+            }
+            i++;
+        }
+    }
 }
